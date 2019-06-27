4498ee14ac6bf99031ed102fb4fb4645c4b79baa
jeninalanzi <jeninalanzi@gmail.com>
2019-06-27 08:40:18 -0500

    Fixed final output on LoanCalculator to pass test

--- 20190627/activities/04-Loan-Calculator/starter-code/LoanCalculator/src/main/java/com/company/LoanCalculator.java
+++ 20190627/activities/04-Loan-Calculator/starter-code/LoanCalculator/src/main/java/com/company/LoanCalculator.java
@@ -9,11 +9,11 @@ public class LoanCalculator {
 
         // Prompt user for their mortgage amount, then save in a float variable.
         System.out.println("Type the amount of your mortgage: ");
-        float mortgageLoan = scan.nextFloat();
+        double mortgageLoan = scan.nextInt();
 
         // Prompt user for the mortgage term in months and save result in integer variable.
         System.out.println("What is the term of the mortgage (in months)? ");
-        int term = scan.nextInt();
+        double term = scan.nextInt();
 
         // Prompt user for the annual interest rate of the mortgage (just type the number of percentage):
         System.out.println("What is the annual interest of the mortgage? (numbers only): ");
@@ -29,10 +29,10 @@ public class LoanCalculator {
 
 
         // Solve for P (fixed monthly payment)
-        double fixedMonthlyPayment = mortgageLoan * (monthlyInterestRate*parens) / (parens -1);
+        double fixedMonthlyPayment = mortgageLoan * ((monthlyInterestRate*parens) / (parens -1));
 
         // Display monthly payment to the user
-        System.out.printf("Your monthly payment is $ %.2f", fixedMonthlyPayment);
+        System.out.println("Your monthly payment is $ " + fixedMonthlyPayment);
 
         // Display loan total to the user
 
