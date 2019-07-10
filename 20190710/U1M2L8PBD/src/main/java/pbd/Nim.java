6dd6eee3176f3ee046a0db5bc1bcf3017a593eec
jeninalanzi <jeninalanzi@gmail.com>
2019-07-10 08:58:28 -0500

    edited Nim.java

--- 20190710/U1M2L8PBD/src/main/java/pbd/Nim.java
+++ 20190710/U1M2L8PBD/src/main/java/pbd/Nim.java
@@ -9,16 +9,6 @@ public class Nim {
         System.out.println("A: " + a + "     B: " + b + "     C: " + c);
     }
 
-    public static String choosePile(String player) {
-        // Prompt user to pick which pile.
-        Scanner scan = new Scanner(System.in);
-        System.out.print(player + ", choose a pile: ");
-        String pileChosen = scan.nextLine();
-
-        return pileChosen;
-    }
-
-
 
     public static void main(String[] args) {
 
@@ -30,9 +20,10 @@ public class Nim {
         int pileA = random.nextInt(10) + 3;
         int pileB = random.nextInt(10) + 3;
         int pileC = random.nextInt(10) + 3;
+        int total = pileA + pileB + pileC;
 
         // A boolean to see who's turn it is.
-        boolean isPlayerOne;
+        boolean isPlayerOne = true;
 
         // BEGIN GAME
         // 1. Prompt the players for their names.
@@ -41,12 +32,64 @@ public class Nim {
         System.out.println("Player 2, enter your name: ");
         String player2 = scan.nextLine();
 
-        // 2. Display the piles
-        displayPiles(pileA, pileB, pileC);
 
-        // 3. Player one starts
-        choosePile(player1);
-            
+        do {
+            // Display the piles
+            displayPiles(pileA, pileB, pileC);
+
+            // 3. Player one starts
+            System.out.println(player1 + ", choose from the pile: ");
+            String playerOneChoice = scan.nextLine();
+
+            System.out.println("How many to remove from pile " + playerOneChoice + "? ");
+            int numberToRemove = Integer.parseInt(scan.nextLine());
+
+            // When player1 makes a number choice, hand turn over to player2
+            if (playerOneChoice.equals("A")) {
+                pileA -= numberToRemove;
+                total -= numberToRemove;
+                isPlayerOne = false;
+            } else if (playerOneChoice.equals("B")) {
+                pileB -= numberToRemove;
+                total -= numberToRemove;
+                isPlayerOne = false;
+            } else if (playerOneChoice.equals("C")) {
+                pileC -= numberToRemove;
+                total -=numberToRemove;
+                isPlayerOne = false;
+            }
+
+            // Display the piles
+            displayPiles(pileA, pileB, pileC);
+
+            // 3. Player two starts
+            System.out.println(player2 + ", choose from the pile: ");
+            String playerTwoChoice = scan.nextLine();
+
+            System.out.println("How many to remove from pile " + playerTwoChoice + "? ");
+            numberToRemove = Integer.parseInt(scan.nextLine());
+
+            // When player2 makes a number choice, hand turn over to player1
+            if (playerTwoChoice.equals("A")) {
+                pileA -= numberToRemove;
+                total -= numberToRemove;
+                isPlayerOne = true;
+            } else if (playerTwoChoice.equals("B")) {
+                pileB -= numberToRemove;
+                total -= numberToRemove;
+                isPlayerOne = true;
+            } else if (playerTwoChoice.equals("C")) {
+                pileC -= numberToRemove;
+                total -=numberToRemove;
+                isPlayerOne = true;
+            }
+        } while (total > 0);
+
+        if (total == 0 && !isPlayerOne) {
+            System.out.println(player2 + " is the winner!");
+        } else if (total == 0 && isPlayerOne) {
+            System.out.println(player1 + " is the winner!");
+        }
 
 
     }
